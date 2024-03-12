5579eaf7f808b34ef2aed9bfc07d4e22bf973a64
Niel McLaren <d@nielmclaren.com>
2024-03-12 12:43:54 -0700

    Remove diagonal speed boost. Clean up.

--- BraitenBoids/main.cpp
+++ BraitenBoids/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-float x;
-float y;
+sf::Clock clockwork;
+
+// Pixels per second.
+float speed = 300.f;
+sf::Vector2f position;
 
 void handleEvent(sf::RenderWindow &window) {
     sf::Event event;
@@ -25,18 +29,26 @@ void handleEvent(sf::RenderWindow &window) {
     }
 }
 
-void handleKeyboard() {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        x -= 3;
+void step(float timeDelta) {
+    sf::Vector2f direction(0, 0);
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        direction.x -= 1;
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        x += 3;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        direction.x += 1;
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        y -= 3;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        direction.y -= 1;
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-        y += 3;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        direction.y += 1;
+    }
+
+    if (direction.x != 0 || direction.y != 0) {
+        float len = sqrt(direction.x * direction.x + direction.y * direction.y);
+        position.x += direction.x / len * speed * timeDelta;
+        position.y += direction.y / len * speed * timeDelta;
     }
 }
 
@@ -44,20 +56,25 @@ void handleKeyboard() {
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 800), "BraitenBoids");
     window.setKeyRepeatEnabled(false);
+    window.setFramerateLimit(60);
 
     sf::Vector2u size = window.getSize();
-    x = size.x / 2;
-    y = size.y / 2;
+    position.x = size.x / 2;
+    position.y = size.y / 2;
 
-    sf::CircleShape shape(100.f);
+    float radius = 20.f;
+    sf::CircleShape shape(radius);
     shape.setFillColor(sf::Color::Green);
-    shape.setOrigin(100.f, 100.f);
+    shape.setOrigin(radius, radius);
 
     while (window.isOpen()) {
-        handleEvent(window);
-        handleKeyboard();
+        sf::Time elapsed = clockwork.getElapsedTime();
+        clockwork.restart();
 
-        shape.setPosition(x, y);
+        handleEvent(window);
+        step(elapsed.asSeconds());
+
+        shape.setPosition(position);
 
         window.clear();
         window.draw(shape);
