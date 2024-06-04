826d6bf23f2d96ac4b2a43d4707859b9588deb2b
Niel McLaren <d@nielmclaren.com>
2024-06-04 10:55:21 -0700

    Perform multiple runs and save them to output/build####/run####/ folders.

--- main_evaluate.cpp
+++ main_evaluate.cpp
@@ -2,10 +2,13 @@
 #include "boid_marshaller.hpp"
 #include <format>
 
-MainEvaluate::MainEvaluate(int argc, char *argv[]) : simulation(800, 800) {
+MainEvaluate::MainEvaluate(int argc, char *argv[])
+    : simulation(800, 800), buildNamer("output/", "build", "/"),
+      runNamer(buildNamer.curr(), "run", "/") {
   std::cout << "evaluate command" << std::endl;
 
   unsigned int numGenerations = parseNumGenerations(argc, argv, 30);
+  unsigned int numRuns = parseNumRuns(argc, argv, 1);
 
   // Seed the random number generator.
   srand(static_cast<unsigned>(time(0)));
@@ -13,45 +16,63 @@ MainEvaluate::MainEvaluate(int argc, char *argv[]) : simulation(800, 800) {
   simRunner = SimRunner::create(simulation);
   simulation.registerEntityListener(simRunner);
 
-  simRunner->resetAgents();
-  simRunner->resetFoodSources();
+  std::string buildPath = buildNamer.curr();
+  std::filesystem::create_directories(buildPath);
 
   totalStopwatch.restart();
-  generationStopwatch.restart();
 
-  std::cout << "Total generations: " << numGenerations << std::endl;
-  for (unsigned int generationIndex = 0; generationIndex < numGenerations;
-       ++generationIndex) {
-    std::cout << "Running generation " << generationIndex << "...";
-    simRunner->resetFoodSources();
+  for (unsigned int run = 0; run < numRuns; ++run) {
+    std::cout << "Starting run " << (run + 1) << " of " << numRuns << std::endl;
 
-    unsigned int stepCount = simRunner->fastForward();
+    std::string runPath = runNamer.curr();
+    std::filesystem::create_directories(runPath);
 
-    unsigned int numFoodSourcesRemaining = simulation.getNumFoodSources();
+    runStopwatch.restart();
 
-    unsigned int foodConsumed =
-        SimRunner::numFoodSources - numFoodSourcesRemaining;
-    float foodConsumedPerStep =
-        stepCount <= 0
-            ? 0
-            : static_cast<float>(foodConsumed) / static_cast<float>(stepCount);
+    simRunner->resetAgents();
 
-    sf::Time elapsed = generationStopwatch.getElapsedTime();
-    std::cout << " complete (" << elapsed.asSeconds() << " seconds, "
-              << stepCount << " steps, " << foodConsumed << " consumed, "
-              << printf("%.2f", foodConsumedPerStep) << " food/step)"
-              << std::endl;
     generationStopwatch.restart();
 
-    evolutionLog.addEntry(simulation, generationIndex, stepCount);
-    simRunner->selectAndMutate();
+    for (unsigned int generation = 0; generation < numGenerations;
+         ++generation) {
+      std::cout << "\tRunning generation " << (generation + 1) << " of "
+                << numGenerations << "...";
+      simRunner->resetFoodSources();
+
+      unsigned int stepCount = simRunner->fastForward();
+
+      unsigned int numFoodSourcesRemaining = simulation.getNumFoodSources();
+
+      unsigned int foodConsumed =
+          SimRunner::numFoodSources - numFoodSourcesRemaining;
+      float foodConsumedPerStep = stepCount <= 0
+                                      ? 0
+                                      : static_cast<float>(foodConsumed) /
+                                            static_cast<float>(stepCount);
+
+      sf::Time elapsed = generationStopwatch.getElapsedTime();
+      std::cout << " complete (" << elapsed.asSeconds() << " seconds, "
+                << stepCount << " steps, " << foodConsumed << " consumed, "
+                << printf("%.2f", foodConsumedPerStep) << " food/step)"
+                << std::endl;
+      generationStopwatch.restart();
+
+      evolutionLog.addEntry(simulation, generation, stepCount);
+      simRunner->selectAndMutate();
+    }
+
+    sf::Time elapsed = runStopwatch.getElapsedTime();
+    std::cout << "\tRun time: " << elapsed.asSeconds() << "s" << std::endl;
+
+    simRunner->saveAgents(runNamer.curr() + "boids.json");
+    evolutionLog.save(runNamer.curr() + "evolution_log.csv");
+    evolutionLog.clear();
+
+    runNamer.next();
   }
 
   sf::Time elapsed = totalStopwatch.getElapsedTime();
   std::cout << "Total time: " << elapsed.asSeconds() << "s" << std::endl;
-
-  BoidMarshaller::save(simulation, "output/boids.json");
-  evolutionLog.save("output/evolution_log.csv");
 }
 
 unsigned int MainEvaluate::parseNumGenerations(int argc, char *argv[],
@@ -65,6 +86,25 @@ unsigned int MainEvaluate::parseNumGenerations(int argc, char *argv[],
         std::cerr << "Missing parameter value for number of generations -g."
                   << std::endl;
         throw "Missing parameter value for number of generations -g.";
+        return defaultValue;
+      }
+      return std::stoi(argv[i + 1]);
+    }
+  }
+  return defaultValue;
+}
+
+unsigned int MainEvaluate::parseNumRuns(int argc, char *argv[],
+                                        unsigned int defaultValue) {
+  if (argc <= 1)
+    return defaultValue;
+
+  for (int i = 0; i < argc; ++i) {
+    if (strcmp(argv[i], "-r") == 0) {
+      if (i + 1 >= argc) {
+        std::cerr << "Missing parameter value for number of runs -r."
+                  << std::endl;
+        throw "Missing parameter value for number of runs -r.";
         return defaultValue;
       }
       return std::stoi(argv[i + 1]);
--- main_evaluate.hpp
+++ main_evaluate.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "evolution_log.hpp"
+#include "file_namer.hpp"
 #include "sim_runner.hpp"
 #include "simulation.hpp"
 #include <SFML/Graphics.hpp>
@@ -10,14 +11,19 @@
 
 class MainEvaluate {
   sf::Clock totalStopwatch;
+  sf::Clock runStopwatch;
   sf::Clock generationStopwatch;
 
   Simulation simulation;
   std::shared_ptr<SimRunner> simRunner;
   EvolutionLog evolutionLog;
 
+  FileNamer buildNamer;
+  FileNamer runNamer;
+
   unsigned int parseNumGenerations(int argc, char *argv[],
                                    unsigned int defaultValue);
+  unsigned int parseNumRuns(int argc, char *argv[], unsigned int defaultValue);
 
 public:
   MainEvaluate(int argc, char *argv[]);
